28dcc69529745538ef7de2f384db62674fe73199
Warren R. Bank <warren.r.bank@gmail.com>
2017-06-21 15:52:24 -0700

    fix test: "simple"

--- tests/js/simple.js
+++ tests/js/simple.js
@@ -5,14 +5,14 @@ list.insert(13, 2, 9, 8, 0);
 console.log(list.toArray());
 
 var arr = ["foo", "bar", "hoge"];
-var strList = new SortedList(arr, {
+var strList = new SortedList({
   compare: "string"
-});
+}, arr);
 
 console.log(strList.toArray());
 
-var list2= new SortedList([2,1,3,4,5], {
+var list2= new SortedList({
   resume : true
-});
+}, [2,1,3,4,5]);
 
 console.log("resume (no checking)", list2.toArray())
